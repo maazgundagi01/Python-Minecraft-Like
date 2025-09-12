84f327c8057a81003edb9dce6d393e1356a8a3aa
Maz Gunn <maazgundagi@gmail.com>
2025-09-12 17:08:52 -0500

    setup LoadModels and LoadLights methods

--- main.py
+++ main.py
@@ -7,25 +7,29 @@ class TheGame(ShowBase): #Extend Showbase class to create the game class
 
     def __init__(self):         #Initializing class
         ShowBase.__init__(self) #Initializing Showbase Class
-
+        
+        self.loadModels()
+        self.setupLights()
     
-        grassBlock = loader.loadModel('assets/grass-block.glb')
-        grassBlock.reparentTo(render) #render is equivalent to document object model, using "reparentTo" on block just attaches it to the screen
-        grassBlock.setPos(0,0,0)        
+    def loadModels(self):
+        
+        self.grassBlock = loader.loadModel('assets/grass-block.glb')
+        self.grassBlock.reparentTo(render) #render is equivalent to document object model, using "reparentTo" on block just attaches it to the screen
+        self.grassBlock.setPos(0,0,0)        
 
-        dirtBlock = loader.loadModel('assets/dirt-block.glb')
-        dirtBlock.reparentTo(render)
-        dirtBlock.setPos(0,2,0)
+        self.dirtBlock = loader.loadModel('assets/dirt-block.glb')
+        self.dirtBlock.reparentTo(render)
+        self.dirtBlock.setPos(0,2,0)
 
-        sandBlock = loader.loadModel('assets/sand-block.glb')
-        sandBlock.reparentTo(render)
-        sandBlock.setPos(2,0,0)
+        self.sandBlock = loader.loadModel('assets/sand-block.glb')
+        self.sandBlock.reparentTo(render)
+        self.sandBlock.setPos(2,0,0)
 
-        stoneBlock = loader.loadModel('assets/stone-block.glb')
-        stoneBlock.reparentTo(render)
-        stoneBlock.setPos(2,2,0)
+        self.stoneBlock = loader.loadModel('assets/stone-block.glb')
+        self.stoneBlock.reparentTo(render)
+        self.stoneBlock.setPos(2,2,0)
 
-
+    def setupLights(self):
         mainLight = DirectionalLight('Main Light')  #Creates instance from DirectionaLight class called main light
         mainLightNodePath = render.attachNewNode(mainLight) #Attaches the instance object to render creating a node
         mainLightNodePath.setHpr(30,-60, 0) #Hpr means Heading, Pitch, Roll. i.e The spatial rotation data.
